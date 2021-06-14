79600826c74cac5509e3fd88af9c8d05f02c5e73
william <alown666@gmail.com>
2021-06-14 13:40:09 +0200

    Commit provisoir car rempli de console.log et autre

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -17,12 +17,20 @@ export default class NewBill {
   }
   handleChangeFile = e => {
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
+    console.log("///////////////")
+    console.log(file)
     const filePath = e.target.value.split(/\\/g)
+    console.log(e.target.value)
+    console.log("///////////////")
+    console.log(filePath)
     const fileName = filePath[filePath.length-1]
-    const fileExtArr = fileName.match(/[^.]+/g)
-    const fileExt = fileExtArr[fileExtArr.length-1]
-    if(fileExt === "jpg" || fileExt === "png" || fileExt === "jpeg") 
-    this.firestore
+    const fileExt = fileName.match(/[^.]+/g).pop()    // Pop retire le dernier élément du tableau fourni par match(l'extension)
+    console.log("///////////////")
+    console.log(fileExt)
+    //const fileExt = fileExtArr.pop()
+    //console.log(fileExt)
+    if(fileExt.toLowerCase().match(/(png|jpg|jpeg)/g))  console.log(this.document.querySelector(`input[data-testid="file"]`))
+/*     this.firestore
       .storage
       .ref(`justificatifs/${fileName}`)
       .put(file)
@@ -30,8 +38,9 @@ export default class NewBill {
       .then(url => {
         this.fileUrl = url
         this.fileName = fileName
-      })
+      }) */
       else this.document.querySelector(`input[data-testid="file"]`).value = ""
+      console.log(this.document.querySelector(`input[data-testid="file"]`))
   }
   handleSubmit = e => {
     e.preventDefault()
