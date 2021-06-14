53dfc3e32a3329561a1ff28f47fc3686eb85902a
william <alown666@gmail.com>
2021-06-14 13:39:11 +0200

    Correction du format de date en "en"

--- src/app/format.js
+++ src/app/format.js
@@ -1,9 +1,10 @@
+const monthArr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 export const formatDate = (dateStr) => {
   const date = new Date(dateStr)
   if(isNaN(date)) return console.error("Erreur de format : "+ dateStr)
-  const ye = new Intl.DateTimeFormat('fr', { year: 'numeric' }).format(date)
-  const mo = new Intl.DateTimeFormat('fr', { month: 'short' }).format(date)
-  const da = new Intl.DateTimeFormat('fr', { day: '2-digit' }).format(date)
+  const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date)
+  const mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(date)
+  const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date)
   const month = mo.charAt(0).toUpperCase() + mo.slice(1)
   return `${parseInt(da)} ${month.substr(0,3)}. ${ye.toString().substr(2,4)}`
 }
@@ -18,7 +19,6 @@ export const formatStatus = (status) => {
       return "Refused"
   }
 }
-const monthArr = ["Jan", "Fév", "Mar", "Avr", "Mai", "Jui", "Jui => Bonjour les incohérences", "Aoû", "Sep", "Oct", "Nov", "Déc"]
 
 export const invertFormatDate = ( dateStr) => {
   let regex = /(?<day>[0-9]{0,2})[ ](?<month>[a-zA-Z]+)[.][ ](?<year>[0-9]{2})/
