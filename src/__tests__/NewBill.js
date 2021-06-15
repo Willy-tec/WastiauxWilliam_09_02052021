8d794333833095f06e3332d23149d913367e360b
william <alown666@gmail.com>
2021-06-15 15:22:20 +0200

    Ajout d'un tesst

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -5,61 +5,46 @@ import { localStorageMock } from "../__mocks__/localStorage.js"
 import firestore from "../app/Firestore";
 
 describe("Given I am connected as an employee", () => {
-  describe("WHEN I am on new bill page and I do not fill the field", () => {
-    test("THEN It should not submit", () => {
+  describe("WHEN I am on new bill page and I  fill the file field", () => {
+    test("THEN It should register the filename", () => {
       
-      jest.mock('../app/firestore')
-      
-      //console.log(document.body.innerHTML)
       const onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({ pathname })
       }
-      //Mock the function
-      firestore.storage = () => ({bills, get: jest.fn().mockResolvedValue()})
 
       Object.defineProperty(window, "localStorage", {
         value: localStorageMock
       });
+
       //Mock a user
       const user = JSON.stringify({
         type: 'Employee'
       })
       window.localStorage.setItem('user', user)
+      
       const html = NewBillUI()
       document.body.innerHTML = html
-      const newBill = new NewBill({document, onNavigate, firestore, localStorage: window.localStorage})
-//console.log(document.body.innerHTML)
-      newBill.fileName = "foo.jpg"
-      const inputFile = screen.getByTestId("file")
       
-      const form = screen.getByTestId("form-new-bill")
+      const newBill = new NewBill({document, onNavigate, firestore: null, localStorage: window.localStorage})
+
+      const fileInputDiv = screen.getByTestId("file")
+
       const handleChangeFile = jest.fn(newBill.handleChangeFile)
       
-      inputFile.addEventListener("change", handleChangeFile)
+      fileInputDiv.addEventListener("change", handleChangeFile)
 
       let file = new File(["foo.jpg"], "foo.jpg", {
         type: "image/jpg"
       });
-      //(["image.png"], "image.png", { type: "image/png" })
 
-      console.log(file)
-      /* fireEvent.change(inputFile, {target : {files : [file]} } ) */
-      fireEvent.change(inputFile, {
+      fireEvent.change(fileInputDiv, {
         target: {
-          files: [new File(["image.jpg"], "image.jpg", { type: "image/jpg" })],
+          files: [file]
         },
       });
-/*       fireEvent.change(getByLabelText(/picture/i), {
-        target: {
-          files: [new File(['(⌐□_□)'], 'chucknorris.png', { type: 'image/png' })],
-        },
-      }) */
-/* 
-      fireEvent.change((form)) 
-      console.log(inputFile[0]) */
-      //expect(handleChangeFile).toHaveBeenCalled()
+
       expect(handleChangeFile).toHaveBeenCalled();
-      expect(inputFile.files[0].name).toBe("foo.jpg");
+      expect(fileInputDiv.files[0].name).toBe("foo.jpg");
     })
     
   })
